8d88033168213bbd25cf46c94c5f3bdba80aad33
Anisshehu <86668933+Anisshehu@users.noreply.github.com>
2026-02-17 15:04:26 +0100

    Add jackpot functionality and game start alert
    Signed-off-by: Anisshehu <86668933+Anisshehu@users.noreply.github.com>

--- assets/assets/js/main.js
+++ assets/assets/js/main.js
@@ -1,1 +1,12 @@
+let jackpot = 12450;
 
+function updateJackpot(){
+    jackpot += Math.floor(Math.random()*50);
+    document.getElementById("jackpotAmount").innerText = "€" + jackpot.toLocaleString();
+}
+
+setInterval(updateJackpot, 3000);
+
+function startGame(){
+    alert("🎰 Game loading... (Premium Demo Version)");
+}
